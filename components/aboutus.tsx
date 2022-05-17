0a96271c6fe0de178524980a16ef690b0813945a
Rahi Khan <rahikhan360@gmail.com>
2022-05-17 09:09:32 +0600

    component: skills completed

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -12,7 +12,7 @@ export const Aboutus: React.FC<Members> = ({ members }) => (
 
     {/* Our Team */}
     <div className="tw-container tw-mx-auto tw-px-11 tw-mt-28 tw-text-center">
-      <h2 className="tw-font-black">Our Team</h2>
+      <h2 className="tw-font-bold">Our Team</h2>
       <div className="tw-mt-10 tw-grid tw-grid-cols-2 tw-gap-6 md:tw-grid-cols-3 lg:tw-grid-cols-4 xl:tw-grid-cols-5 lg:tw-gap-20">
         {members.map(member => (
           <Member id={member.id} name={member.name} socialId={member.socialId} link={member.link} key={member.id} />
